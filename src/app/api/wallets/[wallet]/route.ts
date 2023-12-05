654361889ebf3843a35fdce2355464712adb3aae
Danny Mulvihill <danny@paella.dev>
2023-12-05 10:48:31 -0800

    comment out unused api route that breaks build

--- src/app/api/wallets/[wallet]/route.ts
+++ src/app/api/wallets/[wallet]/route.ts
@@ -1,8 +1,8 @@
-import { callCrossmintAPI } from '@/app/utils/crossmint';
-import { NextRequest, NextResponse } from 'next/server';
+import { callCrossmintAPI } from "@/app/utils/crossmint";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: { wallet: string } }) {
-  const apiPath = `v1-alpha1/wallets/polygon:${params.wallet}/nfts`;
-  const apiResponse = await callCrossmintAPI(apiPath, { method: "GET" });
-  return NextResponse.json(apiResponse);
-}
+// export async function GET(req: NextRequest, { params }: { params: { wallet: string } }) {
+//   const apiPath = `v1-alpha1/wallets/polygon:${params.wallet}/nfts`;
+//   const apiResponse = await callCrossmintAPI(apiPath, { method: "GET" });
+//   return NextResponse.json(apiResponse);
+// }
